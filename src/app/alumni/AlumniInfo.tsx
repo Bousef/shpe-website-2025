1793f0e1331883b5168398d84e7c446ad56c3684
schaconbz01 <sebasch01@DESKTOP-U2FTE3A.localdomain>
2025-06-07 06:14:46 -0400

    Components and Page Updated

---
+++ src/app/alumni/AlumniInfo.tsx
@@ -0,0 +1,28 @@
+import { type Member } from "~/app/_components/MemberCard";
+
+
+
+
+export const alumniList: Member[] = [
+    {
+        name: "Hernan Hernandez-Garcia",
+        role: "EX-PRESIDENT",
+        picture: "/alumni/hernan.jpg",
+        linkedin: "https://www.linkedin.com/in/hernanhernan/",
+    },
+    {
+        name: "Vimalys Rivera Caceres",
+        role: "EX-EXTERNAL VICE PRESIDENT",
+        picture: "/alumni/vimalys.jpg",
+        linkedin: "https://www.linkedin.com/in/vimalys-rivera-caceres/",
+    },
+    {
+        name: "Ariana Rodriguez Velez",
+        role: "EX-INTERNAL VICE PRESIDENT",
+        picture: "/alumni/ariana.jpg",
+        linkedin: "https://www.linkedin.com/in/arodriguezvelez/",
+    },
+    //to add more alumni later
+
+];
+
