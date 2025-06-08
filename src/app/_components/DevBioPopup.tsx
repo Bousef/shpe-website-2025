fbf544de3741442378c9804630e5123179828683
Anna Zheng <anna.zheng@ucf.edu>
2025-06-07 23:09:29 -0400

    redo bio pfp size so no forehead get cut off

--- src/app/_components/DevBioPopup.tsx
+++ src/app/_components/DevBioPopup.tsx
@@ -33,7 +33,7 @@ export function DevBioPopup ({ member, onClose }: BioPopupsProps) {
 				{/* bio content goes here */}
 				<div className="flex flex-col md:flex-row gap-4">
 					{/* profile image */}
-					<div className="w-full md:w-1/3 aspect-[3/4] md:h-auto">
+					<div className="w-full md:w-1/3 h-60 overflow-hidden flex-shrink-0">
 						<img
 							src={member.pfp}
 							alt={member.name}
