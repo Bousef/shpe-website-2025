0593bd538d4c698d3774b0138202a2225a0feee2
luciano093 <luparedes100@gmail.com>
2025-06-16 09:33:11 -0400

    Remove password from createMember procedure

--- src/trpc/member.ts
+++ src/trpc/member.ts
@@ -12,8 +12,6 @@ import { db } from "src/server/db";
 import { members } from "src/server/db/schema";
 import { z } from "zod";
 import { eq } from "drizzle-orm";
-import bcrypt from "bcryptjs";   //encrypting of password
-import { idText } from "typescript";
 
 export const memberRouter = createTRPCRouter({
 
@@ -25,7 +23,6 @@ export const memberRouter = createTRPCRouter({
       first_name: z.string(),
       last_name: z.string(),
       email: z.string().email(),
-      password: z.string(),
     })
   )
   .mutation(async ({ input }) => {
@@ -37,15 +34,12 @@ export const memberRouter = createTRPCRouter({
     if (existingMember.length > 0) {
       throw new Error("Error: Account already exists.");
     }
-
-    const hashedPassword = await bcrypt.hash(input.password.slice(0, 50), 10); //must hash password once a new member is created
     
     //insert new member with the hashed password
     const newMember = await db
     .insert(members)
     .values({
       ...input,
-      password: hashedPassword, //only thing that changes is hashed password
     })
     .returning();
 
