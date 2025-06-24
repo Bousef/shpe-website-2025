f90a3a27281a007e6a55b4872f853f82fe1920cc
luciano093 <luparedes100@gmail.com>
2025-06-24 18:46:42 -0400

    Add api to create alumni

--- src/server/api/routers/alumni.ts
+++ src/server/api/routers/alumni.ts
@@ -1,5 +1,41 @@
-import { createTRPCRouter } from "~/server/api/trpc";
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { alumni } from "~/server/db/schema";
+import { eq } from "drizzle-orm";
 
 export const AlumniRouter = createTRPCRouter({
+    createAlumni: publicProcedure
+        .input(
+            z.object({
+                first_name: z.string(),
+                last_name: z.string(),
+                grad_year: z.number(),
+                image: z.string(),
+                position: z.string(),
+                linkedIn: z.string(),
+            })
+        )
+        .mutation(async ({ input, ctx }) => {
+            const existingAlumni = await ctx.db
+            .select()
+            .from(alumni)
+            .where(eq(alumni.first_name, input.first_name).append(eq(alumni.last_name, input.last_name)));
 
+            if (existingAlumni.length > 0) {
+                // Alumni already exists
+                return null;
+            }
+            
+            const newAlumni = (await ctx.db
+                .insert(alumni)
+                .values(input)
+                .returning())[0];
+
+            if (!newAlumni) {
+                console.error("Tried to create an alumni, but database returned null. Check if the database is set up correctly.");
+                return null;
+            }
+
+            return newAlumni;
+        }),
 });
