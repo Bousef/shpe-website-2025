95203fa6546a91dfa7da432b0915b4edada62124
FernandoAilon <fernandoailon26@yahoo.com>
2025-06-24 17:32:18 -0400

    changed schema a bit

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -35,12 +35,10 @@ export const alumni = createTable(
     id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
     first_name: d.varchar({length: 100}),
     last_name: d.varchar({length: 100}),
-    email: d.varchar({length:100}).unique().notNull(),
-    image: d.varchar({length: 2048}),
-    bio: d.text(),
-    resume: d.varchar({length: 2048}),
-    job: d.varchar({length: 100}),
     grad_year: d.integer(),
+    image: d.varchar({length: 2048}),
+    postion: d.text(),
+    linkedIn: d.text(),
 
   })
 
