a68fc6019c99333562519b6c822b721f0ba4674b
Anna Zheng <anna.zheng@ucf.edu>
2025-06-26 18:56:53 -0400

    remove bio from member

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -23,7 +23,6 @@ export const members = createTable(
     last_name: d.varchar({ length: 100 }),
     email: d.varchar({ length: 100 }).unique().notNull(),
     image: varchar({ length: 2048 }), //url
-    bio: text(),
     resume: varchar({ length: 2048 }), //url
     is_member: boolean().default(false),
   })
