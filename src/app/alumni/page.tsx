7fc53dfa09a247d4633dce6ea78fe68fd0aeb7d7
luciano093 <luparedes100@gmail.com>
2025-06-26 21:38:10 -0400

    Add search bar

--- src/app/alumni/page.tsx
+++ src/app/alumni/page.tsx
@@ -3,17 +3,20 @@
 import Navbar from "../_components/NavBar";
 import AlumniCard from "../_components/AlumniCard";
 import { alumniList } from "./AlumniInfo";
+import SearchBar from "./_components/search-bar";
+import { api } from "~/trpc/server";
 
 export default function Alumni() {
-
+  
   return (
-
     <div className="min-h-screen bg-gradient-to-b from-white to-blue-100">
       <Navbar />
 
       <div className="text-center py-10">
         <h1 className="text-4xl text-blue-800">2024 - 2025</h1>
       </div>
+
+      <SearchBar initialQuery="" />
 
       <div className="grid grid-cols-0 sm:grid-cols-3 md:grid-cols-3 gap-6 px-6 pb-20 justify-items-center">
         {alumniList.map((member) => (
