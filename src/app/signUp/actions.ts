ea76f64eda9a87cffb4acf7d98888db3b32f6057
FernandoAilon <fernandoailon26@yahoo.com>
2025-07-01 21:56:31 -0400

    adding an actions file for signup

---
+++ src/app/signUp/actions.ts
@@ -0,0 +1,32 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '~/server/auth/server'
+
+
+export async function signup(prevState: unknown, formData: FormData): Promise<{error: string}> {
+  const supabase = await createClient();
+
+  if (!supabase) {
+    return {error: 'Supabase client is not initialized.'};
+  }
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signUp(data);
+
+  if (error) {
+    console.error('Signup error:', error);
+    return {error: error.message};
+  }
+
+  revalidatePath('/', 'layout');
+  redirect('/');
+}
