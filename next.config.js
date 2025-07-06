fb112292f2eb0f9d9cd2825ced0cdb5ba9cd241a
luciano093 <luparedes100@gmail.com>
2025-07-06 13:08:56 -0400

    Make supabase client environmental variables optional

--- next.config.js
+++ next.config.js
@@ -8,8 +8,15 @@ import { env } from "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
-    remotePatterns: [new URL(env.NEXT_PUBLIC_SUPABASE_URL + "/**/*")],
+    remotePatterns: [],
   },
 };
+
+const supabaseUrl = env.NEXT_PUBLIC_SUPABASE_URL;
+
+// I don't know why I have to make so many checks for the config object, but my linter complains otherwise.
+if (supabaseUrl && config.images && config.images.remotePatterns) {
+  config.images.remotePatterns.push(new URL(supabaseUrl + "/**/*"));
+}
 
 export default config;
