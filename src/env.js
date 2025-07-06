d9b1720276f5a361a9824061225602e919666882
luciano093 <luparedes100@gmail.com>
2025-07-06 13:08:56 -0400

    Make supabase client environmental variables optional

--- src/env.js
+++ src/env.js
@@ -24,8 +24,14 @@ export const env = createEnv({
    * Specify your client-side environment variables schema here.
    */
   client: {
-    NEXT_PUBLIC_SUPABASE_URL: z.string(),
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+    NEXT_PUBLIC_SUPABASE_URL:
+      process.env.NODE_ENV === "production"
+      ? z.string()
+      : z.string().optional(),
+    NEXT_PUBLIC_SUPABASE_ANON_KEY:
+      process.env.NODE_ENV === "production"
+      ? z.string()
+      : z.string().optional(),
   },
 
   /**
