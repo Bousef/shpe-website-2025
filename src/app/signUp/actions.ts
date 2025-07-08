20ef95024231830d5aae6b8116840af233f355a2
FernandoAilon <fernandoailon26@yahoo.com>
2025-07-07 21:06:05 -0400

    finally added email in use error, that works

--- src/app/signUp/actions.ts
+++ src/app/signUp/actions.ts
@@ -1,12 +1,13 @@
 'use server'
 
+import { error } from 'console';
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
 import { createClient } from '~/server/auth/server'
 
 
-export async function signup(prevState: unknown, formData: FormData): Promise<{error: string}> {
+export async function signup(prevState: unknown, formData: FormData): Promise<{error?: string}> {
   const supabase = await createClient();
 
   if (!supabase) {
@@ -15,17 +16,33 @@ export async function signup(prevState: unknown, formData: FormData): Promise<{e
 
   // type-casting here for convenience
   // in practice, you should validate your inputs
-  const data = {
+  const data2 = {
     email: formData.get('email') as string,
     password: formData.get('password') as string,
   }
 
-  const { error } = await supabase.auth.signUp(data);
+  
+
+
+  const { error, data} = await supabase.auth.signUp(data2)
+
+  if((data.user?.identities?.length === 0 || !data.user?.identities) && data.user?.app_metadata.provider === 'email'){
+    console.error('Signup error: E-mailaddress already in use');
+    return {error: 'email already exists.'};
+  }
+
+  if (data.user?.identities?.length === 0 || !data.user?.identities) {
+    console.error('Signup error: User already exists.');
+    return {error: 'User already exists.'};
+  }
+
+
 
   if (error) {
     console.error('Signup error:', error);
     return {error: error.message};
   }
+
 
   revalidatePath('/', 'layout');
   redirect('/');
