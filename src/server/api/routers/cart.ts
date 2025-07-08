c7f8c1ac66024d8b5459907c966de3b1dbada29b
luciano093 <luparedes100@gmail.com>
2025-07-08 17:27:26 -0400

    Modify addProduct api to automatically use authorized member's uuid

--- src/server/api/routers/cart.ts
+++ src/server/api/routers/cart.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { cart } from "~/server/db/schema";
+import { sql } from "drizzle-orm";
 
 export const cartRouter = createTRPCRouter({
   addProduct: protectedProcedure.input(
       z.object({
-        member_id: z.number(),
         product_id: z.number(),
         quantity: z.number().min(1).max(1000).default(1),
       })
@@ -13,9 +13,14 @@ export const cartRouter = createTRPCRouter({
         const { db } = ctx;
 
         const product = await db.insert(cart).values({
-          member_id: input.member_id,
+          member_uuid: ctx.user.id,
           product_id: input.product_id,
           quantity: input.quantity,
+        }).onConflictDoUpdate({
+          target: [cart.member_uuid, cart.product_id],
+          set: {
+            quantity: sql`${cart.quantity} + ${input.quantity}`,
+          },
         }).returning();
 
         return product;
