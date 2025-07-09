60870be905ad31306efb5a5560a56e1fc10435a3
luciano093 <luparedes100@gmail.com>
2025-07-08 20:12:09 -0400

    Revert timeline to before the overwrite

---
+++ src/lib/search-parser/tokenizer.ts
@@ -0,0 +1,126 @@
+import type { Token } from "./tokens";
+
+type Tokenizer = {
+    tokens: Token[];
+    query: string;
+    current: number;
+    start: number;
+}
+
+export function tokenize(query: string): Token[] {
+    const tokenizer: Tokenizer = {
+        tokens: [],
+        query,
+        current: 0,
+        start: 0,
+    }
+
+    while (!isAtEnd(tokenizer)) {
+        tokenizer.start = tokenizer.current;
+
+        scanToken(tokenizer);
+    }
+
+    return tokenizer.tokens;
+}
+
+function scanToken(tokenizer: Tokenizer): void {
+    const char = tokenizer.query[tokenizer.current];
+
+    switch (char) {
+        case '"':
+            advance(tokenizer);
+            scanQuotedString(tokenizer);
+            break;
+        case ' ':
+        case '\t':
+        case '\n':
+            advance(tokenizer);
+            break;
+        case '|':
+            tokenizer.tokens.push({ type: 'or', value: '|' });
+            advance(tokenizer);
+            break;
+        case '-':
+            tokenizer.tokens.push({ type: 'exclude', value: '-' });
+            advance(tokenizer);
+            break;
+        case '(':
+            tokenizer.tokens.push({ type: 'leftParen', value: '(' });
+            advance(tokenizer);
+            break;
+        case ')':
+            tokenizer.tokens.push({ type: 'rightParen', value: ')' });
+            advance(tokenizer);
+            break;
+        case ':': {
+            tokenizer.tokens.push({ type: 'colon', value: ':' });
+            advance(tokenizer);
+            break;
+        }
+        default:
+            scanPlainToken(tokenizer);
+    }
+}
+
+function scanQuotedString(tokenizer: Tokenizer): void {
+    const start = tokenizer.current;
+    
+    while(peek(tokenizer) != "\"" && !isAtEnd(tokenizer)) {
+        advance(tokenizer);
+    }
+
+    if(!match(tokenizer, "\"")) {
+        tokenizer.current = start; // Reset to start if no closing quote found
+        while (!isAtEnd(tokenizer) && peek(tokenizer) !== ' ') {
+            advance(tokenizer);
+        }
+    }
+
+    addToken(tokenizer, 'phrase', String(tokenizer.query.slice(tokenizer.start + 1, tokenizer.current - 1)));
+}
+
+function scanPlainToken(tokenizer: Tokenizer): void {
+    while (!isAtEnd(tokenizer) && ![' ', '|', '-', '"', ')', ":"].includes(peek(tokenizer))) {
+        advance(tokenizer);
+    }
+
+    const tokenValue = tokenizer.query.slice(tokenizer.start, tokenizer.current);
+
+    addToken(tokenizer, 'plain', String(tokenValue));
+}
+
+function isAtEnd(tokenizer: Tokenizer): boolean {
+    return tokenizer.current >= tokenizer.query.length;
+}
+
+function advance(tokenizer: Tokenizer): string {
+    if (isAtEnd(tokenizer)) {
+        return ''; // Return empty string if at end
+    }
+
+    const character = tokenizer.query[tokenizer.current];
+    tokenizer.current++;
+    return character!;
+}
+
+function peek(tokenizer: Tokenizer): string {
+    if (isAtEnd(tokenizer)) {
+        return ''; // Return empty string if at end
+    }
+    return tokenizer.query[tokenizer.current]!;
+}
+
+function match(tokenizer: Tokenizer, expected: string): boolean {
+    if (isAtEnd(tokenizer) || tokenizer.query[tokenizer.current] !== expected) {
+        return false;
+    }
+
+    advance(tokenizer);
+
+    return true;
+}
+
+function addToken(tokenizer: Tokenizer, type: string, value: string): void {
+    tokenizer.tokens.push({ type: type as 'phrase' | 'or' | 'exclude', value });
+}
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,5 +1,5 @@
-import { relations, sql } from "drizzle-orm";
-import { index, pgEnum, pgTableCreator, primaryKey, pgTable, varchar, numeric, timestamp, integer, boolean, text } from "drizzle-orm/pg-core";
+import { pgEnum, pgTableCreator, varchar, boolean, unique } from "drizzle-orm/pg-core";
+import type { InferSelectModel } from "drizzle-orm";
 
 // taken from https://supabase.com/docs/guides/auth/identities
 // should probably be moved to a separate file
@@ -11,36 +11,64 @@ export const createTable = pgTableCreator(
   (name) => `shpe-website-2025_${name}`,
 );
 
+// types
+export const positionEnumValues = [
+  "President",
+  "Internal Vice President",
+  "Corporate Vice President",
+  "Secretary",
+  "Marketing Vice President",
+  "Treasurer",
+  "Technology Chair",
+  "Professional Development Chair",
+  "Projects Chair",
+  "Mentorship Chair",
+  "Outreach Chair",
+  "Shpetinas Chair",
+  "Social Chair",
+  "Director",
+  "DevTeam",
+  "Committee",
+  "Member",
+] as const;
+export type Position = typeof positionEnumValues[number];
+export const positionEnum = pgEnum('position', positionEnumValues);
+
+export type Alumni = InferSelectModel<typeof alumni>;
+
 //--------------------  Tables --------------------
 
 //members table
 export const members = createTable(
   "members",
   (d) => ({
-    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+    // id has to mirror the id from Supabase's auth.users table
+    // at the moment it isn't possible to reference it directly due to the way drizzle-orm works
+    // so we have to assert it in the code that creates the members
+    uuid: d.uuid().primaryKey().notNull(),
     ucf_id: d.integer().unique().notNull(),
     first_name: d.varchar({ length: 100 }),
     last_name: d.varchar({ length: 100 }),
     email: d.varchar({ length: 100 }).unique().notNull(),
     image: varchar({ length: 2048 }), //url
-    bio: text(),
     resume: varchar({ length: 2048 }), //url
     is_member: boolean().default(false),
+    position: positionEnum("position").default("Member"),
   })
 );
 
-//alumni table
 export const alumni = createTable(
   "alumni",
   (d) => ({
-    //id
-    //first name
-    //last name
-    //year
-    //position
-    //linkedin 
-    //image
+    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+    first_name: d.varchar({length: 100}),
+    last_name: d.varchar({length: 100}),
+    grad_year: d.varchar({length: 100}),
+    image: d.varchar({length: 2048}),
+    linkedIn: d.text(),
+    position: positionEnum("position").default("Member"),
   })
+
 );
 
 export const products = createTable(
@@ -51,9 +79,23 @@ export const products = createTable(
     category: d.varchar({ length: 100 }).unique().notNull(),
     image: varchar({ length: 2048 }), //url
     description: varchar({ length: 2048 }), 
-    price: d.real(),
+    price: d.real().notNull(),
     stock: d.integer(),
   })
+);
+
+export const cart = createTable(
+  "cart",
+  (d) => ({
+    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+    created_at: d.timestamp({ withTimezone: true }).defaultNow(),
+    member_uuid: d.uuid().notNull().references(() => members.uuid),
+    product_id: d.integer().notNull().references(() => products.id),
+    quantity: d.integer().notNull(),
+  }), (table) => [
+    unique().on(table.member_uuid, table.product_id),
+    unique("cart_member_product_unique").on(table.member_uuid, table.created_at),
+  ]
 );
 
 /*
