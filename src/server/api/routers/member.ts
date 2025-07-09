cf50bde28a9fdf05f058211ed31c127897c60a36
luciano093 <luparedes100@gmail.com>
2025-07-09 10:19:52 -0400

    Fix trpc member router to use uuid instead of id

--- src/server/api/routers/member.ts
+++ src/server/api/routers/member.ts
@@ -19,6 +19,7 @@ export const memberRouter = createTRPCRouter({
   createMember: publicProcedure
   .input(
     z.object({
+      uuid: z.string().uuid(),
       ucf_id: z.number(),
       first_name: z.string(),
       last_name: z.string(),
@@ -74,19 +75,19 @@ export const memberRouter = createTRPCRouter({
 
   //delete member - debugging
   deleteMember: publicProcedure
-  .input(z.object({id: z.number()}))
+  .input(z.object({uuid: z.string().uuid()}))
   .mutation(async ({input}) => {
     const delteMem = await db
       .select()
       .from(members)
-      .where(eq(members.id, input.id));
-    
+      .where(eq(members.uuid, input.uuid));
+
     if (delteMem.length == 0){
       throw new Error("No member with that Id");
     }
 
 
-    await db.delete(members).where(eq(members.id, input.id));
+    await db.delete(members).where(eq(members.uuid, input.uuid));
 
 
     return delteMem[0];
