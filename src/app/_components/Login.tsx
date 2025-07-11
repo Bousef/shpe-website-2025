38858f750ae29e136c196bdb696d87c0c72aa5a0
luciano093 <luparedes100@gmail.com>
2025-07-10 21:31:26 -0400

    Refactor signup and login pages

--- src/app/_components/Login.tsx
+++ src/app/_components/Login.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { useActionState } from 'react';
-import { login, signup } from '../login/actions';
+import { login } from '../login/actions';
 import Link from 'next/link';
+import InputBox, { InputField, PasswordInputField } from './InputBox';
 
 export default function Login(){
 	const [loginState, loginAction] = useActionState(login, { error: "" });
-	const [signupState, signupAction] = useActionState(signup, { error: "" });
 
-	const currentError = loginState.error || signupState.error;
+	const currentError = loginState.error;
 
 	return (
 		<section className="flex flex-col items-center pb-[6rem] py-[2rem] px-4 min-w-[280px]">
@@ -19,35 +19,25 @@ export default function Login(){
 			</div>
 
 			<form className="w-full max-w-md space-y-1">
-				{/* border for the form container */}
-				<div className="border-4 border-[var(--shpe-light-blue)] p-4 divide-y-2">
-					{/* EMAIL */}
-					<div className="pb-3">
-							<div className="h-6 text-red-500 text-sm hidden">
-								{currentError && <p>{currentError}</p>}
-							</div>
-							{/* <label htmlFor="email" className="block text-[var(--shpe-blue)] mb-1">Email</label> */}
-							<input 
-								id="email" 
-								name="email" 
-								type="email" 
-								placeholder="Email"
-								required 
-								className="w-full text-[var(--shpe-navy-blue)] text-xl focus:outline-none  "
-							/>
-					</div>
-					{/* PASSWORD */}
-					<div className="pt-3">
-						<input 
-							id="password" 
-							name="password" 
-							type="password" 
-							placeholder="Password"
-							required 
-							className="w-full text-[var(--shpe-navy-blue)] text-xl focus:outline-none"
-						/>
-					</div>
-				</div>
+				<InputBox>
+				<InputField 
+					id="email"
+					type="email"
+					name="email"
+					required
+					placeholder="UCF Email"
+				 />
+				 <PasswordInputField
+				 	id='password'
+					name="password"
+					required
+					placeholder='Password'
+				 />
+				</InputBox>
+
+				{currentError && (
+					<div className="text-red-600 text-sm mb-1">{currentError}</div>
+				)}
 
 				{/* REMEMBER ME + FORGET PASSWORD */}
 				<div className="flex justify-between items-center mb-2">
