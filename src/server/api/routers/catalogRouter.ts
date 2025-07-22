7cf6a74ffddc959cd4886705f0d414efa543c6cb
luciano093 <luparedes100@gmail.com>
2025-07-22 12:56:02 -0400

    Add catalog related apis to catalog router

--- src/server/api/routers/catalogRouter.ts
+++ src/server/api/routers/catalogRouter.ts
@@ -1,8 +1,32 @@
 import { legacyClient } from "~/lib/square/client";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 import { z } from "zod";
+import { FileWrapper, type CreateCatalogImageRequest, type UpdateCatalogImageRequest } from "square/legacy";
 
 export const catalogRouter = createTRPCRouter({
+    createCatalogImage: publicProcedure.input(
+        z.object({
+            request: z.custom<CreateCatalogImageRequest>().optional(),
+            imageFile: z.instanceof(FileWrapper).optional(),
+        })
+    ).query(async ({ input }) => {
+        return await legacyClient.catalogApi.createCatalogImage(input.request, input.imageFile);
+    }),
+
+    updateCatalogImage: publicProcedure.input(
+        z.object({
+            imageId: z.string(),
+            request: z.custom<UpdateCatalogImageRequest>().optional(),
+            imageFile: z.instanceof(FileWrapper).optional(),
+        })
+    ).query(async ({ input }) => {
+        return await legacyClient.catalogApi.updateCatalogImage(input.imageId, input.request, input.imageFile);
+    }),
+
+    catalogInfo: publicProcedure.query(async () => {
+        return await legacyClient.catalogApi.catalogInfo();
+    }),
+    
     listCatalog: publicProcedure.input(
         z.object({
             cursor: z.string().optional(),
