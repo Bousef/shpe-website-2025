295d106fb49d698e1c29decef8ff8f584c8f1495
luciano093 <luparedes100@gmail.com>
2025-07-27 05:21:28 -0400

    Add cloneOrder api

--- src/server/api/routers/orders.ts
+++ src/server/api/routers/orders.ts
@@ -212,6 +212,33 @@ export const ordersRouter = createTRPCRouter ({
             return response.order;
         }),
 
+    cloneOrder: publicProcedure
+        .input(
+            z.object({
+                orderId: z.string(),
+                version: z.number(),
+                idempotencyKey: z.string(),
+            })
+        ).query(async ({ input }) => {
+            const response = await squareClient.orders.clone({
+                ...input
+            });
+
+            // in case of an error, we accumulate all errors into one string and throw them
+            // this should probably be handled better...
+            if (response.errors && response.errors?.length > 0) {
+                throw Error(response.errors.reduce((acc, val) => 
+                    acc + val.detail + "\n"
+                , ""))
+            }
+
+            if (response.order === undefined) {
+                throw Error("cloneOrder returned an undefined order. This should not happen.");
+            }
+
+            return response.order;
+        }),
+
         
     updateOrders: publicProcedure
         .input(
