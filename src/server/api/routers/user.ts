ba3545fb864cb401f593208db1bb924ce4972ac7
FernandoAilon <fernandoailon26@yahoo.com>
2025-08-08 19:33:24 -0400

    created an api that retrieves the current order of a user

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -2,10 +2,50 @@ import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 import { cartRouter } from "./cart";
 import { members } from "~/server/db/schema";
 import { eq } from "drizzle-orm";
+import { squareClient } from "~/lib/square/client";
 
 /// Represents the currently signed-in user.
 export const userRouter = createTRPCRouter({
-  cart: cartRouter,
+  retrieveCurrentOrder: publicProcedure
+        .query(async({ctx}) => {
+          if (!ctx.supabase) return null;
+
+          const { data: { user } } = await ctx.supabase.auth.getUser();
+
+          if (!user) return null;
+          
+          const member = await ctx.db
+          .select()
+          .from(members)
+          .where(eq(members.uuid, user.id));
+
+          const currentMember =  member[0] ?? null;
+
+          const currentOrder = await squareClient.orders.search({
+            locationIds: [currentMember?.square_customer_id ?? ""], 
+            query:{
+              filter:{
+                stateFilter:{
+                  states: ["OPEN" , "DRAFT"]
+                }
+              },
+              sort:{
+                sortField: "CREATED_AT",
+                sortOrder: "DESC"
+              }
+            },
+            limit: 1
+          })
+          if (currentOrder.errors && currentOrder.errors.length > 0) {
+              throw Error(currentOrder.errors.reduce((acc, val) => acc + val.detail + "\n", ""));
+          }
+
+          if (!currentOrder.orders) {
+              throw Error("retrieveCurrentOrder returned an undefined orders.");
+          }
+
+          return currentOrder.orders[0] ?? null;
+        }),
 
   getCurrentMember: publicProcedure.query(async ({ ctx }) => {
     if (!ctx.supabase) return null;
@@ -30,4 +70,7 @@ export const userRouter = createTRPCRouter({
     await ctx.supabase.auth.signOut();
     return { success: true };
   }),
+
+
+
 });
