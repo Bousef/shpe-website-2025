bfcb660518778ae44fc44d2b96c05c912d0a82ed
luciano093 <luparedes100@gmail.com>
2025-08-11 21:30:06 -0400

    Implement option to update items in cart

--- src/app/cart/_components/CartItems.tsx
+++ src/app/cart/_components/CartItems.tsx
@@ -33,6 +33,29 @@ export default function CartItems({ order, refetch }: { order: Order, refetch: (
         });
     }
 
+    const updateItemQuantity = (itemId: string, quantity: number) => {
+        console.log("Old fields: ", order.lineItems);
+        const updatedOrder = order.lineItems?.map(item => {
+            if (item.uid === itemId) {
+                return {
+                    ...item,
+                    quantity: quantity.toString(),
+                };
+            }
+            return item;
+        });
+
+        if (!order.id) return;
+
+        updateOrder.mutate({
+            orderId: order.id,
+            order: {
+                ...order,
+                lineItems: updatedOrder,
+            },
+        });
+    }
+
     if (isLoading) {
         return <div>Loading...</div>;
     }
@@ -58,28 +81,27 @@ export default function CartItems({ order, refetch }: { order: Order, refetch: (
                     className="w-20 h-20 object-contain bg-gray-100 rounded"
                 />
                 <div className="flex-1">
-                <h2 className="font-semibold">{item.name}</h2>
-                <p className="text-gray-600">${(item.totalMoney?.amount ?? 0n) / 100n}</p>
-                <div className="mt-2 flex items-center gap-2">
-                    <label className="text-sm">Qty:</label>
-                    <select
-                    defaultValue={item.quantity}
-                    // onChange={(e) => {
-                    //   const newQuantity = parseInt(e.target.value);
+                    <h2 className="font-semibold">{item.name}</h2>
+                    <p className="text-gray-600">${(item.totalMoney?.amount ?? 0n) / 100n}</p>
+                    <div className="mt-2 flex items-center gap-2">
+                        <label className="text-sm">Qty:</label>
+                        <select defaultValue={item.quantity}
+                            className="border px-2 py-1"
+                            onChange={(e) => {
+                                const newQuantity = parseInt(e.target.value);
 
-                    //   item.quantity = newQuantity;
+                                if (!item.uid) return;
 
-                    //   updateQuantityAction(item.id, newQuantity);
-                    // }}
-                    className="border px-2 py-1"
-                    >
-                    {[...Array<number>(10)].map((_, i) => (
-                        <option key={i + 1} defaultValue={i + 1}>
-                        {i + 1}
-                        </option>
-                    ))}
-                    </select>
-                </div>
+                                updateItemQuantity(item.uid, newQuantity);
+                            }}
+                        >
+                        {[...Array<number>(10)].map((_, i) => (
+                            <option key={i + 1} defaultValue={i + 1}>
+                            {i + 1}
+                            </option>
+                        ))}
+                        </select>
+                    </div>
                 </div>
 
                 <button
