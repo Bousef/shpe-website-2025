66268cb3501c88ee496ee51e1f6549db5c031e9d
cataon03 <catalinaocampon03@gmail.com>
2025-08-19 14:16:15 -0400

    adding about and global

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import AboutSection from "../_components/AboutSection";
+import Board from "../_components/Board";
+export default function AboutPage () {
+
+    return (
+        <div>
+            <AboutSection/>
+        </div>
+    );
+}
