3b03b018bdf3609960b6490233aaa3febc0049a0
adanjoserrojas <adanrojas224@gmail.com>
2026-02-06 19:45:23 -0500

    Admin only client

---
+++ src/app/admin/components/clientAdmin.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { motion, AnimatePresence } from "motion/react";
+import { useState, useEffect } from "react";
+
+export default function AdminUI() {
+
+    return (
+        <div></div>
+    );
+};
