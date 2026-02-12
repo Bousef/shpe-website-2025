741c679c6c058ad28392abc700fb8db697365c93
adanjoserrojas <adanrojas224@gmail.com>
2026-02-12 10:41:49 -0500

    Modify profile data UI

--- src/app/profile/components/clientProfile.tsx
+++ src/app/profile/components/clientProfile.tsx
@@ -77,6 +77,7 @@ export default function ClientProfile() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15A2.25 2.25 0 002.25 6.75v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z" />
                 </svg>
             ),
+
         },
     ];
 
@@ -169,6 +170,7 @@ export default function ClientProfile() {
                                         {field.value ?? "—"}
                                     </p>
                                 </div>
+                                <button className="h-[30px] w-auto rounded-xl bg-[#001f5b] text-white cursor-pointer text-center leading-[30px] px-3">Modify</button>
                             </motion.div>
                         ))}
                     </div>
